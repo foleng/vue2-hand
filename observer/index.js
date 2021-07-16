d700044e9771dc93bc114a71e3921b31ce191639
huangyi <huangyi@tungee.com>
2021-07-16 18:03:39 +0800

    feat: 监听器

--- observer/index.js
+++ observer/index.js
@@ -1,9 +1,9 @@
 /*
- * :file description: 
+ * :file description: 监听器
  * :name: \vue2-hand\observer\index.js
  * :author: 黄艺
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-07-16 17:49:06
  * :last editor: 黄艺
- * :date last edited: 2021-07-16 17:49:06
+ * :date last edited: 2021-07-16 18:03:19
  */
